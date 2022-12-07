6cc1ecf4d809c8691e67ca7f38fa13f0d214cbc7
Louis Dimmock <louis.dimmock@tag-games.com>
2022-12-07 15:51:33 +0000

    feat: Day 5 Part 1

---
+++ AdventOfCodeDay5/AdventOfCodeDay5/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCodeDay5
+{
+    class Program
+    {
+        private struct Stack
+        {
+            public List<string> Crates;
+        }
+
+        private const int k_tabSize = 4;
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            var stacks = new List<Stack>();
+
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("move"))
+                {
+                    ProcessMoveRequest(line, ref stacks);
+                }
+                else
+                {
+                    ProcessStackMap(line, ref stacks);
+                }
+            }
+
+            for(var i = 0; i < stacks.Count; ++i)
+            {
+                Console.Write(stacks[i].Crates[0]);
+            }
+        }
+
+        private static void ProcessMoveRequest(string instruction, ref List<Stack> stacks)
+        {
+            var expression = new System.Text.RegularExpressions.Regex(
+                @"move (?<Count>[^,]+) from (?<From>[^,]+) to (?<To>[^,]+)");
+            
+            var match = expression.Match(instruction);
+
+            var count = int.Parse(match.Groups["Count"].Value);
+            var from = int.Parse(match.Groups["From"].Value) - 1;
+            var to = int.Parse(match.Groups["To"].Value) - 1;
+
+            var fromStack = stacks[from].Crates;
+            var toStack = stacks[to].Crates;
+
+            for (var i = 0; i < count; ++i)
+            {
+                toStack.Insert(0, fromStack[0]);
+                fromStack.RemoveAt(0);
+            }
+        }
+
+        private static void ProcessStackMap(string line, ref List<Stack> stacks)
+        {
+            var lineEntries = GetStacksInLine(line);
+
+            if (stacks.Count == 0)
+            {
+                CreateBaseStacks(lineEntries.Length, ref stacks);
+            }
+
+            for (var i = 0; i < lineEntries.Length; ++i)
+            {
+                var index = lineEntries[i].IndexOf('[');
+                if (index == -1)
+                    continue;
+
+                var letter = lineEntries[i].Substring(index + 1, 1);
+                
+                stacks[i].Crates.Add(letter);
+            }
+        }
+
+        private static void CreateBaseStacks(int count, ref List<Stack> stacks)
+        {
+            for (var i = 0; i < count; ++i)
+            {
+                stacks.Add(new Stack()
+                {
+                    Crates = new List<string>()
+                });
+            }
+        }
+
+        private static string[] GetStacksInLine(string line)
+        {
+            var entries = new string[(line.Length + 1) / k_tabSize];
+            var index = 0;
+
+            for (int i = 0; i < line.Length; i += k_tabSize)
+            {
+                entries[index] = line.Substring(i, Math.Min(k_tabSize, line.Length-i));
+                index++;
+            }
+
+            return entries;
+        }
+    }
+}
