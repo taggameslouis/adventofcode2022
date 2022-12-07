ff4d8e8296917f208f895ea37fbedbc377379617
Louis Dimmock <louis.dimmock@tag-games.com>
2022-12-07 12:22:00 +0000

    feat: Day 3 Part 2

--- Day3/AdventOfCodeDay3/Program.cs
+++ Day3/AdventOfCodeDay3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -8,19 +9,24 @@ namespace AdventOfCodeDay3
     {
         private const int k_lowercaseOffset = 'a' - 1;
         private const int k_uppercaseOffset = 'A' - 27;
-        
+        private const int k_groupSize = 3;
+
         static void Main(string[] args)
         {
             var sum = 0;
-            
+
             var lines = File.ReadAllLines("input.txt");
+
+            var currentGroup = new List<string>();
             foreach (var line in lines)
             {
-                var length = line.Length / 2;
-                var first = line.Substring(0, length);
-                var second = line.Substring(length, length);
+                currentGroup.Add(line);
 
-                var intersections = first.Intersect(second);
+                if (currentGroup.Count < k_groupSize)
+                    continue;
+
+                var intersections = FindIntersectionsForGroup(currentGroup);
+                currentGroup.Clear();
 
                 foreach (var intersection in intersections)
                 {
@@ -28,8 +34,22 @@ namespace AdventOfCodeDay3
                     sum += count;
                 }
             }
-            
+
             Console.WriteLine($"Sum: {sum}");
+        }
+
+        private static List<char> FindIntersectionsForGroup(List<string> currentGroup)
+        {
+            var uniqueList = currentGroup[0].ToList();
+            
+            for(var i = 1; i < currentGroup.Count; ++i)
+            {
+                var list = currentGroup[i].ToList();
+
+                uniqueList = uniqueList.Intersect(list).ToList();
+            }
+
+            return uniqueList;
         }
     }
 }
